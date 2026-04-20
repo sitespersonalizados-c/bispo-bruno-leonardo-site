f4248639ccc957fb23e179fc634906f830f83b5e
sitespersonalizados-c <cristinadev80@gmail.com>
2026-04-20 09:51:49 -0300

    Refactor renderizarDestaques function for additional class

--- assets/js/script-videos.js
+++ assets/js/script-videos.js
@@ -32,8 +32,9 @@ function renderizarDestaques (dados) {
 
       if (index === 0) classe = 'card-video-iluminados';
       else if (index === 1) classe = 'card-video-efata';
-      else if (index === 2) classe = 'card-video-oculto';
-      else if (index === 3) classe = 'card-video-daniel';
+      else if (index === 2) classe = 'card-video-efata2';
+      else if (index === 3) classe = 'card-video-oculto';
+      else if (index === 4) classe = 'card-video-daniel';
       else classe = 'card-video-iluminados';
 
       // 1. Verificamos se o link está vazio
